8d92493209e2d3a4275f5da2cfe4cc51b9f6bb96
Darth Vve <osheviremajoroh@gmail.com>
2025-06-18 01:38:36 +0100

    v0.4: implementing home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className='container'>
-
-    </main>
-  );
+    return (
+        <main className='container'>
+            <section className="hero">
+                <h1>Hi my name is <span>Vire Majoroh</span></h1>
+                <h2 className="typewriter">I&apos;m a Software Engineer.</h2>
+                <p>What is Research Methodology? Research methodology is the specific procedures or techniques used to identify, select, process, and analyze information about a topic. In a research paper, the methodology section allows the reader to critically evaluate a study&apos;s overall validity and reliability</p>
+                <div className="hero-buttons">
+                    <button>Download CV</button>
+                </div>
+            </section>
+        </main>
+    );
 }
